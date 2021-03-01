d0c369118cb51ae0c3a4713067c1a81649eb0ff9
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-03-01 16:57:00 +0100

    :bulb: MAJ programmePrincipal.py

--- programmePrincipal.py
+++ programmePrincipal.py
@@ -1,46 +1,63 @@
 #!/usr/bin/python3
 #-*- coding: utf8 -*-
 """
+## Programme principal
+
+Détermine le type de chaque image à tester selon :
+
+* le plus proche voisin contenu dans un fichier de 200 images de références
+
+* les 5 plus proches voisins contenus dans un fichier de 200 images de références
     
 """
 pass
-
-# On fait les imports nécessaires selon le contexte
 
 # Pour mesurer le temps de traitement du script
 from datetime import datetime 
 # Pour copier les variable
 import copy 
 
-# Modules knn
+# Module knn
 from knn.distance import *
 from knn.plusProchesVoisins import *
 
 # Script principal
 def main():    
     # On récupère la date au début du traitement
-    start_time = datetime.now()    
-    train = open("ressources/training","r")
-    test = open("ressources/testing","r")    
+    start_time = datetime.now()
 
+    # On ouvre le fichier avec les images de références    
+    train = open("ressources/training","r")
+    #On récupère chaque ligne du fichier sous forme d'un tableau de string
     l = train.readlines()
     listetraining = [ligne.split() for ligne in l] 
-          
-    l2 = test.readlines()
-    listetesting = [ligne.split() for ligne in l2]      
 
+    # On ouvre le fichier avec les images à tester
+    test = open("ressources/testing","r")    
+    #On récupère chaque ligne du fichier sous forme d'un tableau de string
+    l2 = test.readlines()
+    listetesting = [ligne.split() for ligne in l2]
+
+    # Affichages de sorties
     print("=============================================================================")    
     print("Type de chaque image selon son plus proche voisin")    
+    print("-------------------------------------------------")    
     print("")
     for j in range(len(listetesting)):
         print("Selon le plus proche voisin, l'image "+str(j+1)+affichage3ou7(plusProcheVoisin(listetesting[j],listetraining)))
+    print("=============================================================================")
+
+    print(".............................................................................")    
+
     print("=============================================================================")    
-    print("=============================================================================")    
-    print("Type de chaque image selon ses 5 plus proches voisins")    
+    print("Type de chaque image selon ses 5 plus proches voisins")
+    print("-----------------------------------------------------")    
     print("")
     for j in range(len(listetesting)):
         print("Selon les 5 plus proches voisins, l'image "+str(j+1)+plusDe3ou7(cinqPlusProcheVoisin(listetesting[j],listetraining)))
     print("=============================================================================")    
+
+    print(".............................................................................")    
 
     # On évalue le temps de traitement
     end_time = datetime.now()
