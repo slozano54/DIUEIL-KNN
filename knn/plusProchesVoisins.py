a17166716be357f4317589b47d91e02de6bd800f
Sébastien LOZANO <sebastien.lozano@ac-nancy-metz.fr>
2021-03-02 16:50:11 +0100

    :white_check_mark: assertions plusDe3ou7()

--- knn/plusProchesVoisins.py
+++ knn/plusProchesVoisins.py
@@ -150,7 +150,7 @@ def affichage3ou7(image:list)->str:
 
     **Postconsitions**
 
-    - la cortie est une chaine qui vaur ' est un 3' ou ' est un  7'
+    - la sortie est une chaine qui vaur ' est un 3' ou ' est un  7'
 
     **Sorties** Une chaine de caractère indiquant ce qu'est l'image, un 3 ou un 7
     """
@@ -183,22 +183,31 @@ def cinqPlusProcheVoisin(image2test:list,imagesref:list)->list:
 
     **Préconditions** 
 
-    - image2test et et les éléments de imagesref ont le même nombre de pixels
-    - image2test et imagesref ont des pixels dont le code est compris entre 0 et 255 à partir du second élément
+    - image2test et les éléments de imagesref sont les listes
+    - image2test et les éléments de imagesref ont le même nombre de pixels
+    - image2test et imagesref ont des pixels dont le code est compris entre 0 et 255 à partir du second élément    
 
-    **Invariant** ...
+    **Postconsitions**
 
-    **Postconsitions** ...
+    - La sortie est un tableau de cinq éléments
+    - chaque élément est une liste dont tous les éléments sauf le premier sont des codes entre 0 et 255
 
-    **Sorties** le plus proche voisin parmi les images de référence
+    **Sorties** les 5 plus proches voisins parmi les images de référence
     """
     pass
 
     # Préconditions
-    # assert isinstance(code1,int),"le premier paramètre n'est pas un entier"
-    # assert isinstance(code2,int),"le second paramètre n'est pas un entier"
-    # if (code1 not in range(256) or code2 not in range(256)):
-    #     raise ValueError("L'un des deux paramètres au moins n'est pas un code de dégradés de gris")
+    assert isinstance(image2test,list),"l'image à tester n'est pas sous forme de liste"    
+    for i in range(len(imagesref)):
+        assert isinstance(imagesref[i],list),"l'image "+str(i+1)+" de l'échantillon de référence n'est pas sous forme de liste"
+        assert len(image2test[1:])==len(imagesref[i][1:]),"image2test l'image "+str(i+1)+" de l'échantillon de référence n'ont pas le même nombre de pixels"
+    for i in range(1,len(image2test)):    
+        if (int(image2test[i]) not in range(256)):
+            raise ValueError("L'un des éléments de imge2test au moins n'est pas un code de dégradés de gris")            
+    for j in range(len(imagesref)):
+        for i in range(1,len(imagesref[j])):
+            if(int(imagesref[j][i]) not in range(256)):
+                raise ValueError("L'un des codes des images de l'échantillon de référence n'est pas un code de dégradé de gris.")
     # Préconditions
     
     references = copy.deepcopy(imagesref)    
@@ -210,43 +219,54 @@ def cinqPlusProcheVoisin(image2test:list,imagesref:list)->list:
         i +=1
     
     # Postconditions
-    # assert isinstance(ecart,int),"l'écart n'est pas un entier"
-    # if (ecart<0):
-    #     raise ValueError("l'écart n'est pas positif")
+    assert len(five_ppv) == 5,"la sortie ne contient pas 5 éléments"
+    for i in range(len(five_ppv)):
+        for j in range(1,len(five_ppv[i])):
+            if (int(five_ppv[i][j]) not in range(256)):
+                raise ValueError("L'un des éléments des cinq plus proches voisins au moins n'est pas un code de dégradés de gris")
     # Postconditions
 
     return five_ppv
 
-def plusDe3ou7(tab:list)->str:
+def plusDe3ou7(five_ppv_image2test:list)->str:
     """
     Fonction qui renvoie une chaine selon la valeur majoritaire parmi 5 plus proches voisins d'une liste à tester
 
     **Paramètres** 
     
-    **Préconditions** ...
+    - five_ppv_image2test la liste des 5 plus proches voisins d'une liste à tester 
+    
+    **Préconditions** 
+    
+    - five_ppv_image2test est une liste de 5 listes
+    - chacune des listes est une liste de codes de gris
+    - le premier éléments de chaque liste est un 3 ou un 7
 
-    **Invariant** ...
+    **Postconsitions**
+    
+    - la sortie est une chaine qui vaur ' est un 3' ou ' est un  7'
 
-    **Postconsitions** ...
 
     **Sorties** Une chaine de caracteres
     """
     pass
 
     # Préconditions
-    # assert isinstance(code1,int),"le premier paramètre n'est pas un entier"
-    # assert isinstance(code2,int),"le second paramètre n'est pas un entier"
-    # if (code1 not in range(256) or code2 not in range(256)):
-    #     raise ValueError("L'un des deux paramètres au moins n'est pas un code de dégradés de gris")
+    assert len(five_ppv_image2test) == 5,"la liste en entrée ne contient pas 5 éléments"
+    for i in range(len(five_ppv_image2test)):
+        assert (five_ppv_image2test[i][0][0] == '3' or five_ppv_image2test[i][0][0] == '7'),"le premier élément de la liste "+str(i+1)+" n'est pas un 3 ou  un 7"
+        for j in range(1,len(five_ppv_image2test[i])):
+            if (int(five_ppv_image2test[i][j]) not in range(256)):
+                raise ValueError("L'un des éléments des cinq éléments de l'entrée au moins n'est pas un code de dégradés de gris")
     # Préconditions
     
     nb_3 = 0
     nb_7 = 0
     maxDe3ou7 = ' est un '
-    for i in range(len(tab)):        
-        if tab[i][0][0]=='3':
+    for i in range(len(five_ppv_image2test)):        
+        if five_ppv_image2test[i][0][0]=='3':
             nb_3 +=1
-        if tab[i][0][0]=='7':
+        if five_ppv_image2test[i][0][0]=='7':
             nb_7 +=1            
     if (nb_3>nb_7):
         maxDe3ou7 +='3'
@@ -254,9 +274,7 @@ def plusDe3ou7(tab:list)->str:
         maxDe3ou7 +='7'
           
     # Postconditions
-    # assert isinstance(ecart,int),"l'écart n'est pas un entier"
-    # if (ecart<0):
-    #     raise ValueError("l'écart n'est pas positif")
+    assert (maxDe3ou7 == " est un 3" or maxDe3ou7 == " est un 7"),"l'écart n'est pas un entier"
     # Postconditions
 
     return maxDe3ou7
@@ -283,4 +301,5 @@ if __name__=="__main__":
     imgMin = ['3', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '38', '43', '105', '255', '253', '253', '253', '253', '253', '174', '6', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '43', '139', '224', '226', '252', '253', '252', '252', '252', '252', '252', '252', '158', '14', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '178', '252', '252', '252', '252', '253', '252', '252', '252', '252', '252', '252', '252', '59', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '109', '252', '252', '230', '132', '133', '132', '132', '189', '252', '252', '252', '252', '59', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '4', '29', '29', '24', '0', '0', '0', '0', '14', '226', '252', '252', '172', '7', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '85', '243', '252', '252', '144', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '88', '189', '252', '252', '252', '14', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '91', '212', '247', '252', '252', '252', '204', '9', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '32', '125', '193', '193', '193', '253', '252', '252', '252', '238', '102', '28', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '45', '222', '252', '252', '252', '252', '253', '252', '252', '252', '177', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '45', '223', '253', '253', '253', '253', '255', '253', '253', '253', '253', '74', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '31', '123', '52', '44', '44', '44', '44', '143', '252', '252', '74', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '15', '252', '252', '74', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '86', '252', '252', '74', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '5', '75', '9', '0', '0', '0', '0', '0', '0', '98', '242', '252', '252', '74', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '61', '183', '252', '29', '0', '0', '0', '0', '18', '92', '239', '252', '252', '243', '65', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '208', '252', '252', '147', '134', '134', '134', '134', '203', '253', '252', '252', '188', '83', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '208', '252', '252', '252', '252', '252', '252', '252', '252', '253', '230', '153', '8', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '49', '157', '252', '252', '252', '252', '252', '217', '207', '146', '45', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '7', '103', '235', '252', '172', '103', '24', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
     #print(isDistanceMin(img2test,imgMin,listetraining))
     #print(plusProcheVoisin(img2test,listetraining))
-    print(affichage3ou7(img2test))
+    #print(affichage3ou7(img2test))
+    #print(plusDe3ou7(cinqPlusProcheVoisin(img2test,listetraining)))
